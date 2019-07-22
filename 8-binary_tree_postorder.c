6dac18fa6fae1e92d081422e4c74faea273c7836
Jason Cortella <419@holbertonschool.com>
2019-07-22 21:40:53 +0000

    Format compliant with Holberton Betty standard

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - goes through a binary tree using post-order traversal
+ * binary_tree_postorder - goes through a binary tree using post-order traverse
  * @tree: tree to traverse
  * @func: pointer to a function to call for each node
  */
