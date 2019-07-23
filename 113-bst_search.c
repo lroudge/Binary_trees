2329c260277f49fb72065eee561415408bedefaa
lroudge <lauradrc@hotmail.fr>
2019-07-23 19:20:00 +0000

    Add task 27

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -10,5 +10,20 @@
  */
 bst_t *bst_search(const bst_t *tree, int value)
 {
+	bst_t *node = (bst_t *)tree;
 
+	if (!tree)
+		return (NULL);
+
+	while (node)
+	{
+		if (value == node->n)
+			return (node);
+		if (value < node->n)
+			node = node->left;
+		else if (value > node->n)
+			node = node->right;
+	}
+
+	return (NULL);
 }
