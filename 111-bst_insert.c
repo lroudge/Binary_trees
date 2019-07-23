abdb33ec04e4077ff9acfbb8a1e039fe1f0e9a30
Jason Cortella <419@holbertonschool.com>
2019-07-23 05:10:18 +0000

    Initial commit p25-p28 - Documentation only

---
+++ 111-bst_insert.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * bst_insert - inserts a value in a Binary Search Tree
+ * @tree: a double pointer to the root node of the BST to insert the value
+ * @value: the value to store in the node to be inserted
+ * Return: A pointer to the created node
+ *         NULL on failure
+ */
+bst_t *bst_insert(bst_t **tree, int value);
+{
+
+}
---
+++ 114-bst_remove.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * bst_remove - removes a node from a Binary Search Tree
+ * @root: a pointer to the root node of the tree where you will remove a node
+ * @value: the value to remove in the tree
+ * Return: a pointer to the new root node of the tree after removal
+ *         NULL on failure
+ */
+bst_t *bst_remove(bst_t *root, int value)
+{
+
+}
