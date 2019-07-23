d029f6acf47d70a0754f3e5137f685bade167d1d
lroudge <lauradrc@hotmail.fr>
2019-07-23 17:54:12 +0000

    Add task 25

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -7,7 +7,38 @@
  * Return: A pointer to the created node
  *         NULL on failure
  */
-bst_t *bst_insert(bst_t **tree, int value);
+bst_t *bst_insert(bst_t **tree, int value)
 {
+	bst_t *tmp = *tree;
+	bst_t *second = NULL;
+	bst_t *new = binary_tree_node(NULL, value);
 
+	if (*tree == NULL)
+		*tree = new;
+
+	while (tmp)
+	{
+		second = tmp;
+		if (value < tmp->n)
+			tmp = tmp->left;
+		else if (value > tmp->n)
+			tmp = tmp->right;
+		else if (value == tmp->n)
+			return (NULL);
+	}
+
+	if (second == NULL)
+		second = new;
+	else if (value < second->n)
+	{
+		second->left = new;
+		new->parent = second;
+	}
+	else
+	{
+		second->right = new;
+		new->parent = second;
+	}
+
+	return (new);
 }
--- binary_trees.h
+++ binary_trees.h
@@ -26,6 +26,9 @@ struct binary_tree_s
 	struct binary_tree_s *right;
 };
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
 
 /* binary_tree_print.c */
 void binary_tree_print(const binary_tree_t *);
