ad610c6434b62b234af39c2850806375e71de032
lroudge <lauradrc@hotmail.fr>
2019-07-23 21:26:32 +0000

    Add task 30 with debug prints

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -9,21 +9,50 @@
  */
 int binary_tree_is_avl(const binary_tree_t *tree)
 {
-	int height_l, height_r;
-
 	if (!tree)
                 return (0);
 
-	if (!binary_tree_is_bst(tree))
-		return (0);
+	return (btia_helper(tree));
+}
 
-	height_l = tree->left ? 1 + binary_tree_is_avl(tree->left) : 0;
-	height_r = tree->right ? 1 + binary_tree_is_avl(tree->right) : 0;
+int btia_helper(const binary_tree_t *tree)
+{
+	int height_l, height_r;
 
-	if (abs(height_l - height_r) > 1)
-		return (0);
-	else
+        if (!tree)
 		return (1);
+	if (!binary_tree_is_bst(tree))
+                return (0);
+
+        height_l = binary_tree_height(tree->right);
+        height_r = binary_tree_height(tree->left);
+
+	printf("Tree level: %d\n", tree->n);
+	printf("height_l = %d\nheight_r = %d\n", height_l, height_r);
+	printf("abs(height_l - height_r) = %d\n", abs(height_l - height_r));
+        if (abs(height_l - height_r) <= 1 && btia_helper(tree->left) && btia_helper(tree->right))
+		return (1);
+	return (0);
+}
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: tree to measure the height of
+ *
+ * Return: height of the tree
+ *         0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height_l = 0;
+	size_t height_r = 0;
+
+	if (!tree)
+		return (0);
+
+	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (height_l > height_r ? height_l : height_r);
 }
 
 /**
