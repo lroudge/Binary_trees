67369f2a2591d6c91c808a77ef4d68da386b3b6c
Jason Cortella <419@holbertonschool.com>
2019-07-23 00:00:54 +0000

    Initial commit p19

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,54 @@
+#include "binary_trees.h"
+
+/**
+ * binary_trees_ancestor - finds the lowest common ancestor of two nodes
+ * @first: a pointer to the first node to find the ancestor
+ * @second: a pointer to the second node to find the ancestor
+ *
+ * Return: pointer to the ancestor node
+ *         NULL if there is no ancestor node
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+				     const binary_tree_t *second)
+{
+	binary_tree_t *root;
+
+	if (!first || !second)
+		return (NULL);
+
+	root = (binary_tree_t *)first;
+	while (root->parent)
+		root = root->parent;
+
+	return (binary_tree_ancestor_helper(root, first, second));
+}
+
+/**
+ * binary_tree_ancestor_helper - finds the lowest common ancestor of two nodes
+ * @root: a pointer to the root node of the binary tree
+ * @first: a pointer to the first node to find the ancestor
+ * @second: a pointer to the second node to find the ancestor
+ *
+ * Return: pointer to the ancestor node
+ *         NULL if there is no ancestor node
+ */
+binary_tree_t *binary_tree_ancestor_helper(binary_tree_t *root,
+					   const binary_tree_t *first,
+					   const binary_tree_t *second)
+{
+	binary_tree_t *lca_l, *lca_r;
+
+	if (root == NULL)
+		return (NULL);
+
+	if (root == first || root == second)
+		return (root);
+
+	lca_l = binary_tree_ancestor_helper(root->left, first, second);
+	lca_r = binary_tree_ancestor_helper(root->right, first, second);
+
+	if (lca_l && lca_r)
+		return (root);
+
+	return (lca_l ? lca_l : lca_r);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -50,6 +50,14 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
+/* Advanced tasks */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+				     const binary_tree_t *second);
+
 /* helper functions */
 int _pow_recursion(int x, int y);
+binary_tree_t *binary_tree_ancestor_helper(binary_tree_t *root,
+					   const binary_tree_t *first,
+					   const binary_tree_t *second);
+
 #endif
