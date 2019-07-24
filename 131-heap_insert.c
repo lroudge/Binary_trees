8e6ed31dc334fb6431d5cf4d7c1adddcc602f563
Jason Cortella <419@holbertonschool.com>
2019-07-24 07:04:02 +0000

    Initial commit p74

--- 131-heap_insert.c
+++ 131-heap_insert.c
@@ -10,5 +10,58 @@
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
+	heap_t *tree, *new, *flip;
+	int size, leaves, sub, bit, level, tmp;
 
+	if (!root)
+		return (NULL);
+	if (!(*root))
+		return (*root = binary_tree_node(NULL, value));
+	tree = *root;
+	size = binary_tree_size(tree);
+	leaves = size;
+	for (level = 0, sub = 1; leaves >= sub; sub *= 2, level++)
+		leaves -= sub;
+	/* subtract all nodes except for bottom-most level */
+
+	for (bit = 1 << (level - 1); bit != 1; bit >>= 1)
+		tree = leaves & bit ? tree->right : tree->left;
+	/*
+	 * Traverse tree to first empty slot based on the binary
+	 * representation of the number of leaves.
+	 * Example -
+	 * If there are 11 nodes in a complete tree, there are 4 leaves on
+	 * the 4th tier of the tree. The first empty slot is the 5th node
+	 * from the left. 5 is 110 in binary. 1 corresponds to left, 0 to right.
+	 * The first empty node is 110 == LLR, *root->left->left->right
+	 */
+
+	new = binary_tree_node(tree, value);
+	leaves & 1 ? (tree->right = new) : (tree->left = new);
+
+	flip = new;
+	for (; flip->parent && (flip->n > flip->parent->n); flip = flip->parent)
+	{
+		tmp = flip->n;
+		flip->n = flip->parent->n;
+		flip->parent->n = tmp;
+	}
+	/* Flip values with parent until parent value exceeds new value */
+
+	return (new);
+}
+
+/**
+ * binary_tree_size - measures the size of a binary tree
+ * @tree: tree to measure the size of
+ *
+ * Return: size of the tree
+ *         0 if tree is NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 }
