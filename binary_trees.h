642f38ca1a8171be3292ebcc84e2c0981e88b0f4
lroudge <lauradrc@hotmail.fr>
2019-07-24 22:56:02 +0000

    Initial commit for task 34

--- binary_trees.h
+++ binary_trees.h
@@ -87,5 +87,6 @@ int btib_helper(const binary_tree_t *tree, int low, int hi);
 bst_t *bst_min_val(bst_t *root);
 int btia_helper(const binary_tree_t *tree, int low, int hi);
 int btih_helper(const binary_tree_t *tree);
+void sata_helper(avl_t **root, int *array, size_t lo, size_t hi);
 
 #endif
