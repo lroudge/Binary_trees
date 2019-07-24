f10110a6b34d97533fa69338276b234d234fe750
Jason Cortella <419@holbertonschool.com>
2019-07-24 07:30:56 +0000

    Initial commit p38

--- 132-array_to_heap.c
+++ 132-array_to_heap.c
@@ -10,5 +10,11 @@
  */
 heap_t *array_to_heap(int *array, size_t size)
 {
+	unsigned int i;
+	heap_t *root = NULL;
 
+	for (i = 0; i < size; i++)
+		heap_insert(&root, array[i]);
+
+	return (root);
 }
